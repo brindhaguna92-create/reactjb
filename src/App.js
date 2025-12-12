89535f03ca284d8a7a1a5c3b3520e87b9ce617cd
Selvakumaran <selvakumarans@acinfotech.com>
2025-12-12 10:38:30 +0530

    first time installation

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+class App extends React.Component
+{
+  render()
+  {
+    return (
+      <>
+      <center>
+      <h1>HI Welcome</h1>
+    <h1>  Learning React js</h1>
+      <h1>thank you</h1></center>
+      <p>Amet mollit consequat excepteurCillum magna voluptate tempor tempor sint mollit magna elit ea consequat ullamco. Sit eu deserunt cillum fugiat proident pariatur laboris. Commodo pariatur irure laborum culpa Lorem. Aliquip eu nisi qui dolore tempor. Incididunt aliqua id occaecat incididunt minim ea fugiat velit eiusmod est tempor aliquip est. Dolore proident exercitation irure eu incididunt ut.Reprehenderit sit ea excepteur voluptate. Veniam fugiat officia dolore commodo ullamco mollit consectetur aliqua magna aute nulla culpa sint. Minim tempor ex nisi pariatur quis culpa irure.Reprehenderit veniam cupidatat deserunt consequat duis eiusmod mollit. Enim ullamco consequat est officia Lorem. Minim nisi irure duis exercitation pariatur proident. Enim elit aute officia minim sit ullamco consequat tempor ex id nostrud.Do Lorem culpa id occaecat exercitation cillum ut. Enim mollit deserunt consectetur officia occaecat et reprehenderit esse sit. In consectetur consectetur sit deserunt ea Lorem do ex velit cupidatat veniam culpa ipsum dolor. Magna non nostrud velit laborum aliquip aliquip laboris. Ullamco velit do aliquip exercitation elit ullamco irure elit occaecat consectetur cillum ea. Est sint consectetur quis nulla ex.
+
+Ut nisi magna excepteur ad laborum ea aliqua aute consectetur nostrud nostrud nisi Lorem deserunt. Ut fugiat et excepteupariatur nisi magna ipsum. Sit elit sunt laboris eiusmod ut veniam ea adipisicing cupidatat non consequat nostrud.
+
+Occaecat incididunt Lorem nisi cupidatat nostrud enim sunt sint quis ut do aute voluptate. Adipisicing cupidatat sunt pariatur aute aliqua exercitation ad et anim fugiat. Velit sit consectetur ullamco excepteur. Exercitation adipisicing proident velit excepteur tempor consectetur irure. Esse qui id sit cillum eu sit dolor. Mollit ut pariatur labore est proident quis proident magna. Voluptate cillum laboris duis pariatur velit amet nisi magna ullamco. tempor nisi. Dolore est aliqua id tempor magna. Ullamco esse incididunt velit reprehenderit deserunt deserunt nulla laboris aliquip. Velit deserunt sint ut tempor veniam occaecat ea ut magna irure consectetur excepteur. Id pariatur officia nostrud laboris duis quis aute amet eiusmod magna minim quis. Do incididunt deserunt quis enim. Incididunt officia exercitation excepteur Lorem dolor velit cupidatat aliqua laborum minim anim.</p>
+      </>
+    )
+  }
+}
+export default App
+
+
+
+
+/*function App()
+{
+  return(
+    <div>
+          <h1>Welcome to React js</h1>
+    <h1>Welcome to React js</h1>
+    <h1>Hello</h1>
+    <h1>Good morning
+
+    </h1>
+
+
+    </div>
+  )
+}
+export default App*/
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
